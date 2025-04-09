978d9f97275719dbaae110d20e6ed72d34449a84
HarshvardhanR <123012249+HarshvardhanR@users.noreply.github.com>
2025-04-09 07:03:37 -0400

    added svgs

--- src/data.jsx
+++ src/data.jsx
@@ -11,32 +11,29 @@ export default{
     leetcode: "https://leetcode.com/u/r_harshv/",
     projects: [
         {
-          title: "Flight Delay Prediction",
-          description: "A brief description of Project One.",
-          image: moon,
-          github: "https://github.com/yourusername/project-one",
-          liveLink: "https://yourwebsite.com/project-one"
-        },
-        {
           title: "EEG Decoding",
-          description: "A brief description of Project Two.",
-          image: uranus,
-          github: "https://github.com/yourusername/project-two",
-          liveLink: "https://yourwebsite.com/project-two"
+          description: "Utilized the BCI Competition III Dataset II (P300 Speller) containing EEG data from two subjects to preprocess signals and evaluate multiple deep learning models such as EEGNet, CNN, LSTM, Transformers, and MAtt using k-fold cross-validation, one-hot encoding, and hyperparameter tuning to explore effective approaches for EEG signal classification.",
+          image: moon,
+          github: "https://github.com/HarshvardhanR/EEG-Decoding",
+          
         },
         {
           title: "World War III",
-          description: "A brief description of Project One.",
-          image: earth,
-          github: "https://github.com/yourusername/project-one",
-          liveLink: "https://yourwebsite.com/project-one"
+          description: "Developed a CLI-based Risk game in Java using Apache Maven for build automation, collaborating with a team of six to implement advanced software design patterns for an abstracted, maintainable codebase, integrate asynchronous log tracing through the Observer pattern for performance monitoring, set up efficient CI/CD pipelines with Git for continuous deployment across three-week sprints, manage and streamline test cases for optimal execution, and document the game's functionalities comprehensively using Javadoc to ensure clear understanding and usability.",
+          image: uranus,
+          github: "https://github.com/HarshvardhanR/World-War-III",
         },
         {
           title: "Banking Management System",
-          description: "A brief description of Project Two.",
+          description: "Designed and developed a secure banking application using Java, the Spring framework, and SQLite for lightweight data storage, featuring a user-friendly GUI, secure authentication, account and profile management, real-time balance and transaction tracking, and functionalities for money transfers, deposits, and withdrawals, with strong emphasis on data integrity, validation, encryption, and efficient database structuring.",
+          image: earth,
+          github: "https://github.com/HarshvardhanR/BankingManagementSystem",
+        },
+        {
+          title: "Flight Delay Prediction",
+          description: "Developed a comprehensive and scalable flight delay prediction system featuring a user-friendly web interface built with HTML, CSS, Bootstrap, and Flask, seamlessly integrating both regression and classification machine learning models to deliver real-time, accurate delay predictions; incorporated extensive feature engineering and model tuning techniques to enhance predictive performance, and designed the system with future integration of airline data sources and additional functionalities in mind.",
           image: mars,
-          github: "https://github.com/yourusername/project-two",
-          liveLink: "https://yourwebsite.com/project-two"
+          github: "https://github.com/HarshvardhanR/Flight-Delay-Prediction",
         },
     ],
 }
